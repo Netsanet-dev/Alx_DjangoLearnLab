fa7597f873152e6f014ed73aaa9b45dbb3ccf91d
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-02-18 10:01:55 +0300

    Adding query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,10 +1,10 @@
 from .models import Author, Book, Library, Librarian
 
-harper = Author.objects.get(name='Harper Lee')
-harper.save()
-harperbook = Book.objects.create(title='To kill a Mockingbird', author=harper)
-harperbook.save()
-Book.objects.get(author=1) 
+# harper = Author.objects.get(name='Harper Lee')
+# harper.save()
+# harperbook = Book.objects.create(title='To kill a Mockingbird', author=harper)
+# harperbook.save()
+# Book.objects.get(author=1) 
 
 nooklibrary = Library.objects.create(name='Book nook')
 nooklibrary.save()
