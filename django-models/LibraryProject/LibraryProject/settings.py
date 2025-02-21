f2046f1fe53415dd11ec39a7e24e17fd151b2360
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-02-21 22:08:11 +0300

    Django Models

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': [BASE_DIR, 'relationship_app/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
