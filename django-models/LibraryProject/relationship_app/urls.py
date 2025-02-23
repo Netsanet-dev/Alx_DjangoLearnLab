9ce5a99ae7ba5e54c1473fb24d035e20ab784764
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-02-23 16:39:41 +0300

    Setting Up auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView, SignUpView
 
 urlpatterns = [
     path('books/', list_books, name='books'),
-    path('library/<int:pk>', LibraryDetailView.as_view(), name="Library")
-    
+    path('library/<int:pk>', LibraryDetailView.as_view(), name="Library"),
+    path('', LoginView.as_view(template_name='relationship_app/login.html'), name='login' ),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', SignUpView.as_view(), name='register')
 ]
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
 from .models import Library, Book
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
+
+# user = User.objects.create_user('Nesanet', 'Nets@gmail.com', 'pass123')
+# user = User.objects.get(username='Nesanet')
 # Create your views here.
+class SignUpView(CreateView):
+   form_class = UserCreationForm
+   success_url = reverse_lazy('login')
+   template_name = 'relationship_app/register.html'
+
+@login_required
 def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
     return render(request, 'relationship_app/list_books.html', context)
+
 
 class LibraryDetailView(DetailView):
     model = Library
