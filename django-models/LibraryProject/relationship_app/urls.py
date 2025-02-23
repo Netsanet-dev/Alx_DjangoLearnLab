60e0b09324cc04ad38388665a1c15325e6053ca5
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-02-23 22:39:16 +0300

    Setting up permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView, LoginView
 from .views import list_books, LibraryDetailView, login_view
 
 urlpatterns = [
     path('books/', list_books, name='books'),
     path('library/<int:pk>', LibraryDetailView.as_view(), name="Library"),
-    path('login/', login_view, name="login")
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html', name='logout')),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html', name='login'))
 ]
 
