a0838e49dbd2961f20cd5dcf3357651e1dee35de
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-02-23 22:46:48 +0300

    Setting up permissions

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,27 +4,17 @@ from django.views.generic import CreateView
 from .models import Library, Book
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
-# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 # from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 
 # user = User.objects.create_user("Netsanet", "Nets@gmail.com", "pass4321")
 
-def login_view(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect("relationship_app/list_books.html")
-    return render(request, "relationship_app/login.html")
-
-
-def logout_view(request):
-    pass
 
 def register(request):
-    return render(request, 'relationshipapp/register.html')
+    form = UserCreationForm()
+    context = {'form', form}
+    return render(request, 'relationshipapp/register.html', context)
 
 
 @login_required
