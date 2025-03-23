36d1cbdaa8a490e2bde6445fabcb8cd2bb4c1255
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-03-23 18:55:11 +0300

    0. Initial Setup and Project Configuration for a Django Blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm, RegisterForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
-def hom(request):
-  return render(request, '')
+def register(request):
+    if request.method == "POST":
+        form = request.POST
+        if form.is_valid():
+            user = RegisterForm(request.user)
+            login(request, user)
+            return redirect("/home")
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {"form": form})
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+def profile(request):
+    return render(request, 'blog/profile.html')
+
+def log_in(request):
+    return render(request, 'registration/login.html')
+
+def log_out(request):
+    return render(request, 'registration/logout.html')
+
+def create_post(request):
+    return render(request, 'blog/post.html')
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-
 # Load Environment Variable
 load_dotenv()
 
@@ -49,7 +48,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "blog/templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,13 +113,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "blog/static",
+    BASE_DIR / 'static',
 ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = '/login'
